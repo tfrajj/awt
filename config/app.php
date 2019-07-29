32f53b6c5b94915142335b06e655a2ae237cb4ae
tfrajj <tfrajj@gmail.com>
2019-07-29 20:30:11 +0300

    Add the Qickadmin package and create the Posts migration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Dev.
+        Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
     ],
 
     /*
